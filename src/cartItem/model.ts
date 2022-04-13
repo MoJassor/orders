6c56764d95be0b5ab8e4d401b5cc2dec90de4e90
mohammad jassor <mohammadjassor>
2022-04-13 15:03:26 +0300

    refactore cartItem and cartItemAccessory model

--- src/cartItem/model.ts
+++ src/cartItem/model.ts
@@ -1,8 +1,26 @@
-import database from "../database/database";
-import { DataTypes } from "sequelize";
+import sequelizeConnection from "../database/database";
 
-const CartItem = database.define(
-  "cartItem",
+import { DataTypes, Model, Optional } from "sequelize";
+
+interface CartItemAttributes {
+  id: number;
+  quantity: number;
+  price: number;
+  deletedAt?: Date;
+}
+export interface CartItemInput extends Optional<CartItemAttributes, "id"> {}
+export interface CartItemOutput extends Required<CartItemAttributes> {}
+
+export default class CartItem
+  extends Model<CartItemAttributes, CartItemInput>
+  implements CartItemAttributes
+{
+  public id!: number;
+  public quantity!: number;
+  public price!: number;
+}
+
+CartItem.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -20,6 +38,10 @@ const CartItem = database.define(
       allowNull: false,
     },
   },
-  { timestamps: false }
+  {
+    tableName: "cartItem",
+    timestamps: false,
+    sequelize: sequelizeConnection,
+    paranoid: true,
+  }
 );
-export default CartItem;
--- src/cartItemAccessory/model.ts
+++ src/cartItemAccessory/model.ts
@@ -1,8 +1,28 @@
-import database from "../database/database";
-import { DataTypes } from "sequelize";
+import sequelizeConnection from "../database/database";
 
-const CartItemAccessory = database.define(
-  "cartItem_accessory",
+import { DataTypes, Model, Optional } from "sequelize";
+
+interface CartItemAccessoryAttributes {
+  id: number;
+  quantity: number;
+  price: number;
+  deletedAt?: Date;
+}
+export interface CartItemAccessoryInput
+  extends Optional<CartItemAccessoryAttributes, "id"> {}
+export interface CartItemAccessoryOutput
+  extends Required<CartItemAccessoryAttributes> {}
+
+export default class CartItemAccessory
+  extends Model<CartItemAccessoryAttributes, CartItemAccessoryInput>
+  implements CartItemAccessoryAttributes
+{
+  public id!: number;
+  public quantity!: number;
+  public price!: number;
+}
+
+CartItemAccessory.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -19,6 +39,10 @@ const CartItemAccessory = database.define(
       allowNull: false,
     },
   },
-  { timestamps: false }
+  {
+    tableName: "cartItemAccessory",
+    timestamps: false,
+    sequelize: sequelizeConnection,
+    paranoid: true,
+  }
 );
-export default CartItemAccessory;
