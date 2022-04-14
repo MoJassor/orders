d4ac2a88b61db09e69bb33d139eb1b4a661cacca
mohammad jassor <mohammadjassor>
2022-04-14 17:25:00 +0300

    add order routes and handler

--- src/cart/route.ts
+++ src/cart/route.ts
@@ -1,15 +1,21 @@
 import { FastifyInstance } from "fastify";
 import CartService from "./service";
-import CartItemService from "../cartItem/service";
 import ProductService from "../product/service";
 import { FastifyReply, FastifyRequest } from "fastify";
 import cartRouteSchema from "./schemas";
 import { verify } from "../utilize/token.service";
 import CartItemAccessoryService from "../cartItemAccessory/service";
 import AccessoryService from "../accessory/service";
+import { Op } from "sequelize";
+import Product from "../product/model";
+import CartItemAccessory from "../cartItemAccessory/model";
+import Accessory from "../accessory/model";
+import CartItem from "../cartItem/model";
+import Status from "../status/model";
+import User from "../user/model";
+import generateQR from "../utilize/qr.generator";
 
 const cartService = new CartService();
-const cartItemService = new CartItemService();
 const accessoryService = new AccessoryService();
 const productService = new ProductService();
 const cartItemAccessoryService = new CartItemAccessoryService();
@@ -17,6 +23,7 @@ const cartItemAccessoryService = new CartItemAccessoryService();
 async function router(app: FastifyInstance) {
   app.addHook("preValidation", verify);
 
+  // add new cart session route
   app.post(
     "/addToCart",
     { schema: cartRouteSchema.addProductToCartSchema },
@@ -37,21 +44,23 @@ async function router(app: FastifyInstance) {
         | any,
       reply: FastifyReply
     ) => {
+      let totalPrice = 0;
       const products = req.body.products;
       const newCart = await cartService.addCart({
         userId: req.user.id,
+        statuesId: 1,
       });
       for (const product of products) {
         const existProduct = await productService.findProductById(
           product.productId
         );
         if (!existProduct) continue;
-        const newCartItem = await cartItemService.addCartItem({
-          cartId: newCart.id,
+        const newCartItem = await (newCart as any).createCartItem({
           productId: existProduct.id,
-          quantity: product.quantity,
-          price: existProduct.price,
+          productQuantity: product.quantity,
+          productPrice: existProduct.price,
         });
+        totalPrice += existProduct.price * product.quantity;
         for (const accessory of product.accessories) {
           const existAccessory = await accessoryService.findAccessoryById(
             accessory.accessoryId
@@ -59,21 +68,22 @@ async function router(app: FastifyInstance) {
           if (!existAccessory) continue;
           await cartItemAccessoryService.addCartItemAccessory({
             accessoryId: accessory.accessoryId,
-            quantity: accessory.quantity,
-            price: existAccessory.price,
+            partQuantity: accessory.quantity,
+            partPrice: existAccessory.price,
             cartItemId: newCartItem.id,
           });
+          totalPrice += existAccessory.price * accessory.quantity;
         }
       }
-
+      newCart.update({ totalPrice });
       reply.code(201).send({ message: "Created" });
     }
   );
 
-  app.get(
-    "/cart/:cartId",
-    { schema: cartRouteSchema.getOneCartSchema },
-
+  // make order route
+  app.post(
+    "/makeOrder/:cartId",
+    { schema: cartRouteSchema.makeOrderSchema },
     async (
       req:
         | FastifyRequest<{
@@ -82,15 +92,173 @@ async function router(app: FastifyInstance) {
         | any,
       reply: FastifyReply
     ) => {
-      const cart = await cartService.findCartById(req.params.cartId, {
-        userId: req.user.id,
+      const cart = await cartService.findCartByPk(req.user.id);
+      if (!cart) return reply.code(404).send({ message: "not found" });
+      if (req.body.amount < cart.totalPrice)
+        return reply.code(405).send({ message: "not found" });
+
+      await cart.update({
+        address: req.body.address,
+        statuesId: 2,
       });
 
-      if (!cart) return reply.code(404).send({ message: "Cart not found" });
-      reply.send({ cart });
+      reply.send({ message: "added to your orders" });
     }
   );
 
+  // get one order route
+  app.get(
+    "/order/:orderId",
+    { schema: cartRouteSchema.getOneOrderSchema },
+    async (
+      req:
+        | FastifyRequest<{
+            Params: { orderId: number };
+          }>
+        | any,
+      reply: FastifyReply
+    ) => {
+      const cart = await cartService.findCartByOptions({
+        where: { id: req.params.orderId, userId: req.user.id },
+        attributes: {
+          exclude: ["updatedAt", "deletedAt", "userId", "statuesId", "address"],
+        },
+        include: [
+          {
+            model: Status,
+            as: "statues",
+            paranoid: false,
+            attributes: ["title"],
+          },
+          {
+            model: CartItem,
+            as: "cartItem",
+            attributes: ["id", "productPrice", "productQuantity"],
+            paranoid: false,
+            include: [
+              {
+                model: CartItemAccessory,
+                as: "productParts",
+                attributes: ["id", "partPrice", "partQuantity"],
+                paranoid: false,
+                include: {
+                  model: Accessory,
+                  as: "accessory",
+                  paranoid: false,
+                  attributes: ["id", "name", "image_url"],
+                },
+              },
+              {
+                model: Product,
+                as: "product",
+                paranoid: false,
+                attributes: ["id", "name", "image_url"],
+              },
+            ],
+          },
+        ],
+      });
+
+      if (!cart) return reply.code(404).send({ message: "Cart not found" });
+      reply.code(200).send(cart);
+    }
+  );
+
+  // get cart details route
+  app.get(
+    "/cart/:cartId",
+    { schema: cartRouteSchema.getOneCartSchema },
+    async (
+      req:
+        | FastifyRequest<{
+            Params: { cartId: number };
+          }>
+        | any,
+      reply: FastifyReply
+    ) => {
+      const cart = await cartService.findCartsByOptions({
+        where: { id: req.params.cartId, userId: req.user.id },
+        attributes: {
+          exclude: ["updatedAt", "deletedAt", "userId", "statuesId", "address"],
+        },
+        include: [
+          {
+            model: Status,
+            as: "statues",
+            paranoid: false,
+            attributes: ["title"],
+          },
+          {
+            model: CartItem,
+            as: "cartItem",
+            attributes: ["id", "productPrice", "productQuantity"],
+            paranoid: false,
+            include: [
+              {
+                model: CartItemAccessory,
+                as: "productParts",
+                attributes: ["id", "partPrice", "partQuantity"],
+                paranoid: false,
+                include: {
+                  model: Accessory,
+                  as: "accessory",
+                  paranoid: false,
+                  attributes: ["id", "name", "image_url"],
+                },
+              },
+              {
+                model: Product,
+                as: "product",
+                paranoid: false,
+                attributes: ["id", "name", "image_url"],
+              },
+            ],
+          },
+        ],
+      });
+      if (!cart) return reply.code(404).send({ message: "Cart not found" });
+      reply.code(200).send(cart[0]);
+    }
+  );
+
+  // get my orders route
+  app.get(
+    "/myOrders",
+    { schema: cartRouteSchema.getMyOrdersSchema },
+    async (
+      req: FastifyRequest | any,
+
+      reply: FastifyReply
+    ) => {
+      const orders = await cartService.findCartsByOptions({
+        where: { userId: req.user.id, statuesId: { [Op.ne]: 1 } },
+
+        include: {
+          model: CartItem,
+          as: "cartItem",
+          attributes: ["id", "productPrice", "productQuantity"],
+          paranoid: false,
+          include: [
+            {
+              model: Product,
+              as: "product",
+              paranoid: false,
+              attributes: ["id", "name", "image_url"],
+            },
+          ],
+        },
+      });
+
+      for (const order of orders) {
+        (order as any).dataValues.QRCode = await generateQR(
+          `${process.env.serverUrl}/api/order/${order.id}`
+        );
+      }
+      reply.code(200).send(orders);
+    }
+  );
+
+  // get my cart items route
   app.get(
     "/myCart",
     { schema: cartRouteSchema.getMyCartSchema },
@@ -100,7 +268,50 @@ async function router(app: FastifyInstance) {
       reply: FastifyReply
     ) => {
       const carts = await cartService.findCartsByOptions({
-        where: { userId: req.user.id },
+        where: { userId: req.user.id, statuesId: 1 },
+        attributes: {
+          exclude: ["updatedAt", "deletedAt", "userId", "statuesId", "address"],
+        },
+        include: [
+          {
+            model: CartItem,
+            as: "cartItem",
+            attributes: ["id", "productPrice", "productQuantity"],
+            paranoid: false,
+            include: [
+              {
+                model: CartItemAccessory,
+                as: "productParts",
+                attributes: ["id", "partPrice", "partQuantity"],
+                paranoid: false,
+                include: {
+                  model: Accessory,
+                  as: "accessory",
+                  paranoid: false,
+                  attributes: ["id", "name", "image_url"],
+                },
+              },
+              {
+                model: Product,
+                as: "product",
+                paranoid: false,
+                attributes: ["id", "name", "image_url"],
+              },
+            ],
+          },
+          {
+            model: Status,
+            as: "statues",
+            paranoid: false,
+            attributes: ["title"],
+          },
+          {
+            model: User,
+            as: "user",
+            paranoid: false,
+            attributes: ["name", "phone"],
+          },
+        ],
       });
       reply.code(200).send(carts);
     }
--- src/cart/schemas.ts
+++ src/cart/schemas.ts
@@ -1,4 +1,43 @@
 export default {
+  makeOrderSchema: {
+    summary: "purchase cart item to be your own",
+    tags: ["Order"],
+    params: {
+      type: "object",
+      required: ["cartId"],
+      properties: {
+        cartId: { type: "number" },
+      },
+    },
+    body: {
+      type: "object",
+      required: ["amount", "address"],
+      properties: {
+        amount: {
+          type: "number",
+        },
+        address: {
+          type: "string",
+        },
+      },
+    },
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          message: { type: "string", example: "the products are yours now !" },
+        },
+      },
+      400: { message: { type: "string", example: "Bad request" } },
+      405: { message: { type: "string", example: "Method not allowed" } },
+      401: { message: { type: "string", example: "unauthorized" } },
+      403: { message: { type: "string", example: "forbidden" } },
+      500: {
+        message: { type: "string", example: "internal server error" },
+      },
+    },
+  },
+
   addProductToCartSchema: {
     summary: "add product to our your cart",
     tags: ["Cart"],
@@ -97,56 +136,50 @@ export default {
     },
   },
 
-  getOneCartSchema: {
-    summary: "see one cart item ",
-    description:
-      "You can view all products in your one cart session and you can delete or update it",
-    tags: ["Cart"],
-    required: ["cartId"],
-    params: { type: "object", properties: { cartId: { type: "number" } } },
+  getOneOrderSchema: {
+    summary: "see one order details",
+    tags: ["Order"],
+    required: ["orderId"],
+    params: { type: "object", properties: { orderId: { type: "number" } } },
     response: {
       200: {
         description: "Successful response",
         type: "object",
         properties: {
-          cart: {
-            type: "object",
-            properties: {
-              id: { type: "number" },
-              cartItem: {
-                type: "array",
-                items: {
+          id: { type: "number" },
+          totalPrice: { type: "number" },
+          cartItem: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                id: { type: "number" },
+                productPrice: { type: "number" },
+                productQuantity: { type: "number" },
+                product: {
                   type: "object",
                   properties: {
                     id: { type: "number" },
-                    price: { type: "number" },
-                    quantity: { type: "number" },
-                    product: {
-                      type: "object",
-                      properties: {
-                        id: { type: "number" },
-                        name: { type: "string" },
-                        image_url: { type: "string" },
-                      },
-                    },
-                    productParts: {
-                      type: "array",
-                      items: {
+                    name: { type: "string" },
+                    image_url: { type: "string" },
+                  },
+                },
+                productParts: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      id: { type: "number" },
+                      accessory: {
                         type: "object",
                         properties: {
                           id: { type: "number" },
-                          accessory: {
-                            type: "object",
-                            properties: {
-                              id: { type: "number" },
-                              name: { type: "string" },
-                              image_url: { type: "string" },
-                            },
-                          },
-                          quantity: { type: "string" },
-                          price: { type: "number" },
+                          name: { type: "string" },
+                          image_url: { type: "string" },
                         },
                       },
+                      partQuantity: { type: "string" },
+                      partPrice: { type: "number" },
                     },
                   },
                 },
@@ -163,6 +196,70 @@ export default {
       },
     },
   },
+
+  getOneCartSchema: {
+    summary: "see one cart item ",
+    description:
+      "You can view all products in your one cart session and you can delete or update it",
+    tags: ["Cart"],
+    required: ["cartId"],
+    params: { type: "object", properties: { cartId: { type: "number" } } },
+    response: {
+      200: {
+        description: "Successful response",
+        type: "object",
+        properties: {
+          id: { type: "number" },
+          totalPrice: { type: "number" },
+          cartItem: {
+            type: "array",
+            items: {
+              type: "object",
+              properties: {
+                id: { type: "number" },
+                productPrice: { type: "number" },
+                productQuantity: { type: "number" },
+                product: {
+                  type: "object",
+                  properties: {
+                    id: { type: "number" },
+                    name: { type: "string" },
+                    image_url: { type: "string" },
+                  },
+                },
+                productParts: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      id: { type: "number" },
+                      accessory: {
+                        type: "object",
+                        properties: {
+                          id: { type: "number" },
+                          name: { type: "string" },
+                          image_url: { type: "string" },
+                        },
+                      },
+                      partQuantity: { type: "string" },
+                      partPrice: { type: "number" },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      400: { message: { type: "string", example: "Bad request" } },
+      401: { message: { type: "string", example: "unauthorized" } },
+      403: { message: { type: "string", example: "Forbidden" } },
+      500: {
+        message: { type: "string", example: "internal server error" },
+      },
+    },
+  },
+
   getMyCartSchema: {
     summary: "see your cart's product ",
     description: "You can view all products in your cart and it's accessories",
@@ -175,14 +272,27 @@ export default {
           type: "object",
           properties: {
             id: { type: "number" },
+            totalPrice: { type: "number" },
+            statues: {
+              type: "object",
+              properties: { title: { type: "string" } },
+            },
+            user: {
+              type: "object",
+              properties: {
+                name: { type: "string" },
+                phone: { type: "string" },
+              },
+            },
             cartItem: {
               type: "array",
               items: {
                 type: "object",
                 properties: {
                   id: { type: "number" },
-                  price: { type: "number" },
-                  quantity: { type: "number" },
+                  productPrice: { type: "number" },
+                  ProductQuantity: { type: "number" },
+                  total: { type: "number" },
                   product: {
                     type: "object",
                     properties: {
@@ -191,23 +301,51 @@ export default {
                       image_url: { type: "string" },
                     },
                   },
-                  productParts: {
-                    type: "array",
-                    items: {
-                      type: "object",
-                      properties: {
-                        id: { type: "number" },
-                        accessory: {
-                          type: "object",
-                          properties: {
-                            id: { type: "number" },
-                            name: { type: "string" },
-                            image_url: { type: "string" },
-                          },
-                        },
-                        quantity: { type: "number" },
-                        price: { type: "number" },
-                      },
+                },
+              },
+            },
+          },
+        },
+      },
+      400: { message: { type: "string", example: "Bad request" } },
+      401: { message: { type: "string", example: "unauthorized" } },
+      403: { message: { type: "string", example: "Forbidden" } },
+      500: {
+        message: { type: "string", example: "internal server error" },
+      },
+    },
+  },
+
+  getMyOrdersSchema: {
+    summary: "see your previous orders",
+    description: "if you want to see order details use (get one order) route",
+    tags: ["Order"],
+    response: {
+      200: {
+        description: "Successful response",
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            id: { type: "number" },
+            totalPrice: { type: "number" },
+            address: { type: "string" },
+            QRCode: { type: "string" },
+            cartItem: {
+              type: "array",
+              items: {
+                type: "object",
+                properties: {
+                  id: { type: "number" },
+                  productPrice: { type: "number" },
+                  productQuantity: { type: "number" },
+                  total: { type: "number" },
+                  product: {
+                    type: "object",
+                    properties: {
+                      id: { type: "number" },
+                      name: { type: "string" },
+                      image_url: { type: "string" },
                     },
                   },
                 },
