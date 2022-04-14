0724e3304851bc6342ebd2dfe84fee8eab875d55
mohammad jassor <mohammadjassor>
2022-04-14 17:11:41 +0300

    edit services fils

--- src/cart/service.ts
+++ src/cart/service.ts
@@ -3,6 +3,7 @@ import CartItemAccessory from "../cartItemAccessory/model";
 import Cart from "./model";
 import Product from "../product/model";
 import Accessory from "../accessory/model";
+import Sequelize from "sequelize";
 
 export default class CartService {
   public addCart = async (input: any) => {
@@ -10,57 +11,14 @@ export default class CartService {
   };
 
   public findCartsByOptions = async (options: any = {}) => {
-    return await Cart.findAll({
-      ...options,
-      include: {
-        model: CartItem,
-        as: "cartItem",
-        attributes: ["id", "price", "quantity"],
-        paranoid: false,
-        include: [
-          {
-            model: CartItemAccessory,
-            as: "productParts",
-            attributes: ["id", "price", "quantity"],
-            paranoid: false,
-            include: { model: Accessory, as: "accessory", paranoid: false },
-          },
-          {
-            model: Product,
-            as: "product",
-            paranoid: false,
-            attributes: ["id", "name", "image_url"],
-          },
-        ],
-      },
-    });
+    return await Cart.findAll(options);
   };
 
   public findCartByOptions = async (options: any) => {
     return await Cart.findOne(options);
   };
 
-  public findCartById = async (id: number, options: any = {}) => {
-    return await Cart.findOne({
-      where: { id, ...options },
-      include: {
-        model: CartItem,
-        as: "cartItem",
-        attributes: ["id", "price", "quantity"],
-        include: [
-          {
-            model: CartItemAccessory,
-            as: "productParts",
-            attributes: ["id", "price", "quantity"],
-            include: [{ model: Accessory, as: "accessory" }],
-          },
-          {
-            model: Product,
-            as: "product",
-            attributes: ["id", "name", "image_url"],
-          },
-        ],
-      },
-    });
+  public findCartByPk = async (id: number) => {
+    return await Cart.findByPk(id);
   };
 }
